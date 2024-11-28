edd6b283da06aa0f186bf3d392087f7116afa147
PeiPei233 <2503585731@qq.com>
2024-11-28 16:34:34 +0800

    change config way

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
+# build your compiler here
+
 make
-
-# if you do not want to run test parallelly, you can set NO_PARALLEL=1
-# export NO_PARALLEL=1
-
-# if you want to use qemu to run test in lab0/lab4/bonus1/bonu2, you can set USE_QEMU=1
-# export USE_QEMU=1
